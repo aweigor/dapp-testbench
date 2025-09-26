587d4bea9c2a26a213e3c47916e80757f07d0d9f
Igor Novitskii <drmnnml@gmail.com>
2025-09-26 15:51:30 +0300

    feat: messenger implementation

--- ethboot-1/contracts/Messenger.sol
+++ ethboot-1/contracts/Messenger.sol
@@ -2,5 +2,20 @@
 pragma solidity ^0.8.28;
 
 contract Messenger {
-  
+  uint public changeCounter;
+
+  address public owner;
+
+  string public theMessage;
+
+  constructor() {
+      owner = msg.sender;
+  }
+
+  function updateTheMessage(string memory _newMessage) public {
+      if(msg.sender == owner) {
+          theMessage = _newMessage;
+          changeCounter++;
+      }
+  }
 }
