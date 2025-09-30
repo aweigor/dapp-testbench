19c05bd036e158db3f2a9c0fb8817439d4d2cc16
Igor Novitskii <drmnnml@gmail.com>
2025-09-30 20:11:16 +0300

    test: foundry test implementation

--- ethboot-2/contracts/SendWithdrawMoney.t.sol
+++ ethboot-2/contracts/SendWithdrawMoney.t.sol
@@ -2,5 +2,61 @@
 pragma solidity ^0.8.28;
 
 import {Test} from "forge-std/Test.sol";
+import {SendWithdrawMoney} from "./SendWithdrawMoney.sol";
 
-contract SendWithdrawMoneyTest is Test {}
+contract SendWithdrawMoneyUser {
+  SendWithdrawMoney sendWithdrawMoney;
+
+  constructor(SendWithdrawMoney sendWithdrawMoney_) public {
+    sendWithdrawMoney = sendWithdrawMoney_;
+  }
+
+  function getUserBalance() public view returns(uint) {
+    return address(this).balance;
+  }
+
+  function withdrawAll() public {
+    sendWithdrawMoney.withdrawAll();
+  }
+}
+
+contract SendWithdrawMoneyTest is Test {
+  SendWithdrawMoney sendWithdrawMoney;
+  address user1;
+  address user2;
+  address payable self;
+
+  function setUp() public {
+    sendWithdrawMoney = createContract();
+    user1 = address(new SendWithdrawMoneyUser(sendWithdrawMoney));
+    user2 = address(new SendWithdrawMoneyUser(sendWithdrawMoney));
+    self = payable(address(this));
+  }
+
+  function createContract() internal returns (SendWithdrawMoney) {
+    return new SendWithdrawMoney();
+  }
+
+  function test_InitialValue() public view {
+    require(sendWithdrawMoney.balanceReceived() == 0, "Initial value of counter should be 0");
+  }
+
+  function test_Deposit() public view {
+    sendWithdrawMoney.deposit{value: 1 ether};
+    require(sendWithdrawMoney.getContractBalance() == 1, "Initial value of counter should be 0");
+  }
+
+  function test_WithdrawAll() public {
+    sendWithdrawMoney.deposit{value: 1 ether};
+    SendWithdrawMoneyUser(user1).withdrawAll();
+    SendWithdrawMoneyUser(user2).withdrawAll();
+    require(SendWithdrawMoneyUser(user1).getUserBalance() == 1, "First user wthdraws all value");
+    require(SendWithdrawMoneyUser(user2).getUserBalance() == 0, "Sencond user has empty withdrawal value");
+  }
+
+  function test_WithdrawToAddress() public {
+    sendWithdrawMoney.deposit{value: 1 ether};
+    sendWithdrawMoney.withdrawToAddress(self);
+    require(self.balance == 1, "Value withdraws to specified address");
+  }
+}
