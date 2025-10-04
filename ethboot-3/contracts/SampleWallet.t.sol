5608ecb9347b8899e11ce84ee67676a75199cdfb
Igor Novitskii <drmnnml@gmail.com>
2025-10-04 21:45:45 +0300

    test: set allowance tests

--- ethboot-3/contracts/SampleWallet.t.sol
+++ ethboot-3/contracts/SampleWallet.t.sol
@@ -27,4 +27,31 @@ contract SampleWalletTest is Test {
   function createContract() internal returns (SampleWallet) {
     return new SampleWallet();
   }
+
+  function test_SetAllowanceSenderRevertTest() public {
+    vm.prank(user1);
+    vm.expectRevert("You are not the owner, aborting!");
+    sampleWallet.setAllowance(user1, 0);
+    assertEq(sampleWallet.allowance(user1), 0);
+  }
+
+  function test_SetAllowanceAllowanceValueAfterRevertTest() public {
+    vm.prank(user1);
+    try sampleWallet.setAllowance(user1, 0) {
+      fail();
+    } catch {
+      assertEq(sampleWallet.allowance(user1), 0);
+      assertEq(sampleWallet.isAllowedToSend(user1), false);
+    }
+  }
+
+  function test_SetAllowanceAllowanceValueAfterUpdateTest() public {
+    vm.prank(self);
+    try sampleWallet.setAllowance(user1, 1) {
+      assertEq(sampleWallet.allowance(user1), 1);
+      assertEq(sampleWallet.isAllowedToSend(user1), true);
+    } catch {
+      fail();
+    }
+  }
 }
